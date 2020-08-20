5c1ad54aa46326674ef0604115d9483fd89a6cd0
Bill <zzlee.tw@gmail.com>
2020-08-20 09:32:17 +0800

    Fixed start-registry scripts

--- start-registry.sh
+++ start-registry.sh
@@ -1,3 +1,7 @@
 #!/bin/sh
 
-docker run --rm -d -p 5000:5000 -v $(pwd)/registry:/var/lib/registry --name registry registry:2
+. ./docker_env.sh
+
+echo Starting registry @ ${DOCKER_HOME}/registry
+
+docker run --rm -d -p 5000:5000 -v ${DOCKER_HOME}/registry:/var/lib/registry --name registry registry:2
