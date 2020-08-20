bfcc684c6881c5fa76022664e8347a08838abbd1
Bill <bill@yuan.com.tw>
2020-08-19 18:52:59 -0700

    Added building scripts for h2_linux_sdk

---
+++ build/h2.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+. /etc/profile
+
+cd /home/zzlee/dev/h2_linux_sdk/ambarella/
+. build/env/aarch64-linaro-gcc.env
+
+cd /home/zzlee/dev/h2
+mkdir build
+cd build
+H2_LINUX_SDK=/home/zzlee/dev/h2_linux_sdk cmake ../cmake
+
+make
---
+++ build/h2_linux_sdk.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+
+. /etc/profile
+
+cd /home/zzlee/dev/h2_linux_sdk/
+./build.sh
+./mk_release.sh
