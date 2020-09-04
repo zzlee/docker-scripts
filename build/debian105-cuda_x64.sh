e729297b334e12103d3777e5d9901bdd306ea6b2
Bill <bill@yuan.com.tw>
2020-09-04 15:46:57 +0800

    Added build script for debian105_x64

---
+++ build/debian105-cuda_x64.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+
+. /etc/profile
+
+TARGET=debian105-cuda_x64
+
+cd /home/zzlee/dev/qcap-dev/qcap
+mkdir build-$TARGET
+cd build-$TARGET
+
+rm lib/* -r
+rm bin/* -r
+cmake .. -DBUILD_TARGET=$TARGET
+make install -j 4
