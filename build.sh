cdeedf759cc7bef0f315c048e9c2461da5a6ca3c
Bill <bill@yuan.com.tw>
2020-09-08 14:11:55 +0800

    Fixed wrong $@ usage in build.sh

--- build.sh
+++ build.sh
@@ -5,5 +5,5 @@ TARGET=$2
 
 shift 2
 
-echo "Building ${TARGET} @ ${IMAGE}..."
-docker run --rm -a stdout -a stderr -v $(pwd)/..:/home/zzlee/dev/ ${IMAGE} /bin/bash -c /home/zzlee/dev/docker-scripts/build/$TARGET.sh $@
+echo "Building ${TARGET} @ ${IMAGE}... $@"
+docker run --rm -a stdout -a stderr -v $(pwd)/..:/home/zzlee/dev/ ${IMAGE} /bin/bash -c "/home/zzlee/dev/docker-scripts/build/${TARGET}.sh $@"
