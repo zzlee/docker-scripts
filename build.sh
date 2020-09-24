97b3ac46d9aedf8ea07fbfa3336b7d761442b6fe
Bill <bill@yuan.com.tw>
2020-09-24 01:39:57 -0700

    Added -it to build.sh

--- build.sh
+++ build.sh
@@ -6,4 +6,4 @@ TARGET=$2
 shift 2
 
 echo "Building ${TARGET} @ ${IMAGE}... $@"
-docker run --rm -a stdout -a stderr -v $(pwd)/..:/home/zzlee/dev/ ${IMAGE} /bin/bash -c "/home/zzlee/dev/docker-scripts/build/${TARGET}.sh $@"
+docker run --rm -it -v $(pwd)/..:/home/zzlee/dev/ ${IMAGE} /bin/bash -c "/home/zzlee/dev/docker-scripts/build/${TARGET}.sh $@"
