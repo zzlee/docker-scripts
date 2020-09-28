23433b025d8f57089221d7c27ea8e592264d8265
Bill <bill@yuan.com.tw>
2020-09-28 01:59:45 -0700

    Added CMAKE_BUILD_TYPE to building scripts

--- build/hi3531d.sh
+++ build/hi3531d.sh
@@ -13,5 +13,5 @@ if [ "$1" = "install" ]; then
 	rm bin/* -r
 fi
 
-HI3531D_SDK_HOME=/home/zzlee/dev/hi3531d-sdk/Hi3531DV100_SDK_V1.0.4.0 cmake .. -DBUILD_TARGET=$TARGET
+HI3531D_SDK_HOME=/home/zzlee/dev/hi3531d-sdk/Hi3531DV100_SDK_V1.0.4.0 cmake .. -DBUILD_TARGET=$TARGET -DCMAKE_BUILD_TYPE=Release
 make $@ -j 4
