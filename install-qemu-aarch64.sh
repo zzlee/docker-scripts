f093d4d6b979f68ed5f7e00e4736d928a0bc8e40
Bill <bill@yuan.com.tw>
2020-09-30 07:11:53 +0000

    Added -y to install-qemu-aarch64.sh

--- install-qemu-aarch64.sh
+++ install-qemu-aarch64.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
-apt-get install qemu binfmt-support qemu-user-static # Install the qemu packages
+apt-get install -y qemu binfmt-support qemu-user-static # Install the qemu packages
 docker run --rm --privileged multiarch/qemu-user-static --reset -p yes # This step will execute the registering scripts
