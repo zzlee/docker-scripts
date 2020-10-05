0847a6aba3414c2fef8383c261d268ff777725f3
Bill <bill@yuan.com.tw>
2020-10-05 06:50:59 +0000

    Added build-all-base-images

---
+++ build-all-base-images.sh
@@ -0,0 +1,8 @@
+#/bin/sh
+
+./build-base-image.sh hisiv
+./build-base-image.sh ubuntu1804_x64
+./build-base-image.sh ubuntu1604_x64
+./build-base-image.sh centos76_x64
+./build-base-image.sh debian105_x64
+./build-base-image.sh ubuntu1804_tx2
