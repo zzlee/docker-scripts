0e89ad8ac7f16ce43e12efdde8c4b721728a9a92
Bill <bill@yuan.com.tw>
2020-10-13 16:51:04 +0800

    Fixed SUDO_USER in build.sh

--- build.sh
+++ build.sh
@@ -6,4 +6,4 @@ TARGET=$2
 shift 2
 
 echo "Building ${TARGET} @ ${IMAGE}... $@"
-./docker-run.sh ${IMAGE} "su ${USER} -c /bin/bash --login -c /docker/docker-scripts/build/${TARGET}.sh $@"
+./docker-run.sh ${IMAGE} "su ${SUDO_USER} -c /bin/bash --login -c /docker/docker-scripts/build/${TARGET}.sh $@"
