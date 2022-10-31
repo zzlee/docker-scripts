3317fba6ed1076e29f7e12e5b812c6b0371e8692
Bill <bill@yuan.com.tw>
2022-10-31 15:36:14 +0800

    Added more features for dicom
    worklist server

--- start-orthanc-server.sh
+++ start-orthanc-server.sh
@@ -4,6 +4,7 @@ HTTP_PORT=8042
 PACS_PORT=4242
 CONFIG_FILE=`pwd`/orthanc.json
 
-docker run -p ${PACS_PORT}:4242 -p ${HTTP_PORT}:8042 --rm \
+docker run -p ${PACS_PORT}:4242 -p ${HTTP_PORT}:8042 --rm -d \
 	-v ${CONFIG_FILE}:/etc/orthanc/orthanc.json \
+	--name orthanic \
 	jodogne/orthanc-plugins
