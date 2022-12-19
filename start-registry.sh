6059593432873bbda2d67e145db704b0fdeecac0
Bill <bill@yuan.com.tw>
2022-12-19 13:23:33 +0800

    Added petalinux & vivado dockerfiles

--- start-registry.sh
+++ start-registry.sh
@@ -5,3 +5,4 @@ DOCKER_HOME=$(pwd)/..
 echo Starting registry @ ${DOCKER_HOME}/registry
 
 docker run --rm -d -p 5000:5000 -v ${DOCKER_HOME}/registry:/var/lib/registry --name registry -e REGISTRY_STORAGE_DELETE_ENABLED=true registry:2
+
