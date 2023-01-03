f622dc52c0c6c7056e534e97b31386ce1255316f
Bill <bill@yuan.com.tw>
2023-01-03 11:40:10 +0800

    Fixed vivado scripts

--- vivado.sh
+++ vivado.sh
@@ -4,7 +4,7 @@ DOCKER_IMAGE=${DOCKER_IMAGE:-yuan88yuan/vivado:v2020.2}
 MAC_ADDR=${MAC_ADDR:-02:42:ac:11:00:03}
 
 xhost +
-../docker-scripts/docker-run.sh \
+./docker-run.sh \
 	--privileged \
 	--mac-address ${MAC_ADDR} \
 	-v /sys/devices/:/sys/devices ${DOCKER_IMAGE} \
