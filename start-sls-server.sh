170a844ca15e51882facac01ec0f9dcaacb8ab74
Bill <bill@yuan.com.tw>
2023-12-25 16:13:35 +0800

    update dockerfile for vivado

--- start-sls-server.sh
+++ start-sls-server.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-docker run --rm -it -p 8080:8080 -p 8080:8080/udp --name sls-server qcap-registry:5000/sls-server:v1
+docker run --rm -it -p 8080:8081 -p 8080:8081/udp --name sls-server yuan88yuan/sls-server:v1
