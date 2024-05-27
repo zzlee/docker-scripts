4759671e7beffbe6c6d3674b2d855f42834febc4
Bill <bill@yuan.com.tw>
2024-05-27 14:24:34 +0800

    Remove used args in docker-daemon.sh

--- docker-daemon.sh
+++ docker-daemon.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 IMAGE=$1
-CONT_NAME=$2
+CONT=$2
 
 docker run -d -t \
 	--workdir /docker \
@@ -10,10 +10,5 @@ docker run -d -t \
 	--env=HOST_GID=${SUDO_GID} \
 	--env=USER=${SUDO_USER} \
 	--volume="/etc/localtime:/etc/localtime:ro" \
-	--env="DISPLAY" \
-	--env="QT_X11_NO_MITSHM=1" \
-	--volume="/tmp/.X11-unix:/tmp/.X11-unix:rw" \
-	--device=/dev/dri \
-	--group-add video \
-	--name $CONT_NAME \
+	--name $CONT \
 	$IMAGE su - ${SUDO_USER}
