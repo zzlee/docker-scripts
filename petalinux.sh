090eccda1b405c7195cd857d896095adbdd38101
Bill <bill@yuan.com.tw>
2024-10-07 13:24:43 +0800

    Added petaliunx/vivado base dockerfile

--- petalinux.sh
+++ petalinux.sh
@@ -1,16 +1,18 @@
 #!/bin/sh
 
-DOCKER_IMAGE=${DOCKER_IMAGE:-yuan88yuan/petalinux:v2020.2}
+IMAGE=${IMAGE:-yuan88yuan/petalinux:v2020.2}
 CONT=${CONT:-petalinux}
+EXTRA_OPTS=${EXTRA_OPTS:-"-v /opt/:/opt/"}
 
-echo DOCKER_IMAGE=${DOCKER_IMAGE}
+echo IMAGE=${IMAGE}
 echo CONT=${CONT}
+echo EXTRA_OPTS=$EXTRA_OPTS
 
 # sysctl -w fs.inotify.max_user_watches="99999999"
 ./docker-run.sh -d -t \
-	-v ~/petalinux-cache:/docker/cache \
-	-v /opt/:/opt/ \
 	--name ${CONT} \
-	${DOCKER_IMAGE} \
+	-v ~/petalinux-cache:/docker/cache \
+	${EXTRA_OPTS} \
+	${IMAGE} \
 	$@
 
--- vivado.sh
+++ vivado.sh
@@ -1,13 +1,15 @@
 #!/bin/sh
 
-DOCKER_IMAGE=${DOCKER_IMAGE:-yuan88yuan/vivado:v2020.2}
+IMAGE=${IMAGE:-yuan88yuan/vivado:v2020.2}
 MAC_ADDR=${MAC_ADDR:-e0:d5:5e:66:22:b7}
 #MAC_ADDR=${MAC_ADDR:-02:42:ac:11:00:03}
 CONT=${CONT:-vivado}
+EXTRA_OPTS=${EXTRA_OPTS:-"-v /opt/:/opt/"}
 
-echo DOCKER_IMAGE=$DOCKER_IMAGE
+echo IMAGE=$IMAGE
 echo MAC_ADDR=$MAC_ADDR
 echo CONT=$CONT
+echo EXTRA_OPTS=$EXTRA_OPTS
 
 xhost +
 ./docker-run.sh -d -t \
@@ -17,5 +19,6 @@ xhost +
 	--name ${CONT} \
 	-v /sys/devices/:/sys/devices \
 	-v /dev/:/dev/ \
-	-v /opt/:/opt/ \
-	${DOCKER_IMAGE} $@
+	${EXTRA_OPTS} \
+	${IMAGE} \
+	$@
