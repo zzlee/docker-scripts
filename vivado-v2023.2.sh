b5e2b6d1ac565c42fa9d5eaf7f2e5763329d7912
Bill <bill@yuan.com.tw>
2026-01-09 09:10:12 +0800

    Rename petalinux-2024.2.sh to petalinux-v2023.2.sh and add vivado-v2023.2.sh

--- vivado-v2023.2.sh
+++ vivado-v2023.2.sh
@@ -1,13 +1,17 @@
 #!/bin/sh
 
 IMAGE=${IMAGE:-yuan88yuan/petalinux:ubuntu2204}
-CONT=${CONT:-petalinux-2024.2}
+CONT=${CONT:-vivado-v2023.2}
 
 echo IMAGE=${IMAGE}
 echo CONT=${CONT}
 
+xhost +
 # sysctl -w fs.inotify.max_user_watches="99999999"
 docker run -itd --rm --name ${CONT} \
+	--volume="/tmp/.X11-unix:/tmp/.X11-unix:rw" \
+        --device=/dev/dri \
+	-e DISPLAY \
 	-v ${PWD}/..:/docker \
 	-v /opt:/opt \
 	-v /etc/timezone:/etc/timezone \
